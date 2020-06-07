6bcfe961d893799d658679074f063136ba348d62
jemc300x <jemc300x@gmail.com>
2020-06-07 08:43:01 -0500

    main added

--- src/components/main.svelte
+++ src/components/main.svelte
@@ -23,18 +23,6 @@
       title="Everything you need in one place"
       content="Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution."
     />
-    <!-- <div class="card-item">
-      <h3><span>01</span> Track company-wide progress</h3>
-      <p class="paragraph">See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.</p>
-    </div>
-    <div class="card-item">
-      <h3><span>02</span> Advanced built-in reports</h3>
-      <p class="paragraph"></p>
-    </div>
-    <div class="card-item">
-      <h3><span>03</span> Everything you need in one place</h3>
-      <p class="paragraph"></p>
-    </div> -->
   </div>
 </section>
 
